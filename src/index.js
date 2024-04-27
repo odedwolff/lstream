8792a284ff1b9cb7918db3de5e5f2b7298c4d1f5
oded wolff <odedwolff@gmail.com>
2024-04-27 19:51:28 +0300

    added ajax skeleton test

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ app.get('/hello', (req, res) => {
   res.send('Santo Cello!');
 });
 
+app.get('/ajaxTest', (req, res) => {
+    res.send('ajaxResponse!');
+  });
+
+
+
+
 // Start the server and listen for incoming requests
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
