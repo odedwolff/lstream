91aa50291ea97aa659afde204068e23b6461d855
oded wolff <odedwolff@gmail.com>
2024-04-27 17:50:26 +0300

    added basic app skeleton for serving static and dynamic content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 
 const app = express();
 
 // Define the port to listen on
 const port = 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '..','public')));
+
 // Simple route that responds with "Hello World!"
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Santo Cello!');
 });
 
