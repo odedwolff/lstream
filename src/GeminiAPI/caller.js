9ddc307d388c3dca026a9a51d7cc34488b64daf5
unknown <odedwolff@gmail.com>
2025-11-02 16:45:35 +0100

    changes model to 1.5 b something

--- src/GeminiAPI/caller.js
+++ src/GeminiAPI/caller.js
@@ -12,7 +12,10 @@ console.log(`genAI = ${JSON.stringify(genAI)}`);
 
 
 //const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
+//const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b"});
+
+
 
 
 
